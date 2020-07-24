9a37818f16882d3c488b92986ef56132a4544b96
John Ericson <John.Ericson@Obsidian.Systems>
2020-07-24 00:13:41 +0000

    Test new show-derivation for CA derivations

--- tests/content-addressed.sh
+++ tests/content-addressed.sh
@@ -8,7 +8,7 @@ clearCache
 export REMOTE_STORE=file://$cacheDir
 
 drv=$(nix-instantiate ./content-addressed.nix --arg seed 1)
-#nix show-derivation --derivation "$drv" --arg seed 1
+nix show-derivation --derivation "$drv" --arg seed 1
 
 #out1=$(nix-build ./content-addressed.nix --arg seed 1)
 #out2=$(nix-build ./content-addressed.nix --arg seed 2)
