9f82e2ba1f5943d74639d8f0121e3478f6b61f24
John Ericson <John.Ericson@Obsidian.Systems>
2020-10-12 22:59:27 +0000

    Add missing experimental features to ipld-derivations test

--- tests/ipld-derivations.sh
+++ tests/ipld-derivations.sh
@@ -14,9 +14,9 @@ simpleFileIpfs
 drv=$(nix-instantiate --experimental-features ca-derivations ./ipfs-derivation-output.nix -A dependent)
 nix --experimental-features 'nix-command ca-derivations' show-derivation --derivation "$drv"
 echo $drv
-cid=$(nix ipld-drv export --derivation $drv)
+cid=$(nix --experimental-features 'nix-command ca-derivations' ipld-drv export --derivation $drv)
 echo cid: $cid
 
-drv2=$(nix ipld-drv import $cid)
+drv2=$(nix --experimental-features 'nix-command ca-derivations' ipld-drv import $cid)
 
 [ $drv = $drv2 ]
