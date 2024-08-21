8e4c541ba029dd19129d3a1c6d628519e37f8e9f
siddhantCodes <siddhantk232@gmail.com>
2024-08-20 21:56:11 -0400

    Convert more end exes 2

--- src/nix/flake.cc
+++ src/nix/flake.cc
@@ -851,7 +851,7 @@ static Strings defaultTemplateAttrPaths = {"templates.default", "defaultTemplate
 struct CmdFlakeInitCommon : virtual Args, EvalCommand
 {
     std::string templateUrl = "templates";
-    Path destDir;
+    std::filesystem::path destDir;
 
     const LockFlags lockFlags{.writeLockFile = false};
 
@@ -877,7 +877,7 @@ struct CmdFlakeInitCommon : virtual Args, EvalCommand
 
     void run(nix::ref<nix::Store> store) override
     {
-        auto flakeDir = absPath(destDir);
+        auto flakeDir = std::filesystem::weakly_canonical(destDir);
 
         auto evalState = getEvalState();
 
@@ -956,7 +956,7 @@ struct CmdFlakeInitCommon : virtual Args, EvalCommand
         }(templateDir, flakeDir);
 
         if (!changedFiles.empty() && std::filesystem::exists(std::filesystem::path{flakeDir} / ".git")) {
-            Strings args = {"-C", flakeDir, "add", "--intent-to-add", "--force", "--"};
+            Strings args = {"-C", flakeDir.string(), "add", "--intent-to-add", "--force", "--"};
             for (auto & s : changedFiles)
                 args.emplace_back(s.string());
             runProgram("git", true, args);
