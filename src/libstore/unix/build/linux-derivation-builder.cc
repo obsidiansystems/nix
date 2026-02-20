50d843ea1851fc221ea8a892eb130dc7fc62b08c
Amaan Qureshi <git@amaanq.com>
2026-02-20 12:16:40 -0500

    derivation-builder: bundle daemon fields into RecursiveNixDaemon struct
    Replaces the `stopDaemon` and `setupRecursiveNixDaemon` free functions with
    a `RecursiveNixDaemon` struct whose `stop()` and `start()` methods reference
    `socket`, `thread`, and `workerThreads` as members. The 3 duplicated fields
    in all 5 derivation builder implementations collapse to a single
    `RecursiveNixDaemon daemon` member. Also folds remaining single-use locals
    (`env`, `inputRewrites`, `tmpDirFd`, etc.) back into the functions that use
    them and removes dead declarations from the builder headers.

--- src/libstore/unix/build/linux-derivation-builder.cc
+++ src/libstore/unix/build/linux-derivation-builder.cc
@@ -11,15 +11,10 @@
 #include "nix/store/globals.hh"
 #include "nix/store/restricted-store.hh"
 #include "nix/store/personality.hh"
-#include "nix/util/linux-namespaces.hh"
-#include "store-config-private.hh"
 
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/resource.h>
-
-#include "nix/util/strings.hh"
-#include "nix/util/signals.hh"
 
 #if NIX_WITH_AWS_AUTH
 #  include "nix/store/aws-creds.hh"
@@ -118,21 +113,11 @@ struct LinuxDerivationBuilder : DerivationBuilder, DerivationBuilderParams
 
     const DerivationType derivationType;
 
-    StringMap env;
-
     std::map<StorePath, StorePath> redirectedOutputs;
 
     OutputPathMap scratchOutputs;
 
-    AutoCloseFD tmpDirFd;
-
-    StringMap inputRewrites, outputRewrites;
-
-    static const std::filesystem::path homeDir;
-
-    AutoCloseFD daemonSocket;
-    std::thread daemonThread;
-    std::vector<std::thread> daemonWorkerThreads;
+    RecursiveNixDaemon daemon;
 
     LinuxDerivationBuilder(
         LocalStore & store, std::unique_ptr<DerivationBuilderCallbacks> miscMethods, DerivationBuilderParams params)
@@ -151,7 +136,7 @@ struct LinuxDerivationBuilder : DerivationBuilder, DerivationBuilderParams
             ignoreExceptionInDestructor();
         }
         try {
-            nix::stopDaemon(daemonSocket, daemonThread, daemonWorkerThreads);
+            daemon.stop();
         } catch (...) {
             ignoreExceptionInDestructor();
         }
@@ -178,11 +163,6 @@ struct LinuxDerivationBuilder : DerivationBuilder, DerivationBuilderParams
     }
 
     friend struct RestrictedStore;
-
-    bool needsHashRewrite()
-    {
-        return true;
-    }
 
     std::filesystem::path tmpDirInSandbox()
     {
@@ -244,18 +224,17 @@ struct LinuxDerivationBuilder : DerivationBuilder, DerivationBuilderParams
         tmpDir = topTmpDir;
         assert(!tmpDir.empty());
 
-        tmpDirFd = AutoCloseFD{open(tmpDir.c_str(), O_RDONLY | O_NOFOLLOW | O_DIRECTORY)};
+        AutoCloseFD tmpDirFd{open(tmpDir.c_str(), O_RDONLY | O_NOFOLLOW | O_DIRECTORY)};
         if (!tmpDirFd)
             throw SysError("failed to open the build temporary directory descriptor %1%", PathFmt(tmpDir));
 
         nix::chownToBuilder(buildUser.get(), tmpDirFd.get(), tmpDir);
 
-        inputRewrites.clear();
-        nix::computeScratchOutputs(store, *this, scratchOutputs, redirectedOutputs, inputRewrites, needsHashRewrite());
+        StringMap inputRewrites;
+        std::tie(scratchOutputs, inputRewrites, redirectedOutputs) =
+            nix::computeScratchOutputs(store, *this, /* needsHashRewrite= */ true);
 
-        nix::initEnv(
-            env,
-            homeDir,
+        auto env = nix::initEnv(
             store.storeDir,
             *this,
             inputRewrites,
@@ -269,24 +248,13 @@ struct LinuxDerivationBuilder : DerivationBuilder, DerivationBuilderParams
         if (drvOptions.useUidRange(drv))
             throw Error("feature 'uid-range' is not supported on this platform");
 
-        if (needsHashRewrite() && pathExists(homeDir))
+        if (pathExists(homeDir))
             throw Error(
                 "home directory %1% exists; please remove it to assure purity of builds without sandboxing",
                 PathFmt(homeDir));
 
         if (drvOptions.getRequiredSystemFeatures(drv).count("recursive-nix"))
-            nix::setupRecursiveNixDaemon(
-                store,
-                *this,
-                *this,
-                addedPaths,
-                env,
-                tmpDir,
-                tmpDirInSandbox(),
-                daemonSocket,
-                daemonThread,
-                daemonWorkerThreads,
-                buildUser.get());
+            daemon.start(store, *this, *this, addedPaths, env, tmpDir, tmpDirInSandbox(), buildUser.get());
 
         nix::logBuilderInfo(drv);
 
@@ -302,7 +270,9 @@ struct LinuxDerivationBuilder : DerivationBuilder, DerivationBuilderParams
             auto awsCredentials = nix::preResolveAwsCredentials(drv);
 #endif
 
-            pid = startProcess([this
+            pid = startProcess([this,
+                                &env,
+                                &inputRewrites
 #if NIX_WITH_AWS_AUTH
                                 ,
                                 awsCredentials
@@ -392,7 +362,7 @@ struct LinuxDerivationBuilder : DerivationBuilder, DerivationBuilderParams
 
         killSandbox(true);
 
-        nix::stopDaemon(daemonSocket, daemonThread, daemonWorkerThreads);
+        daemon.stop();
 
         nix::logCpuUsage(store, drvPath, buildResult, status);
 
@@ -409,14 +379,12 @@ struct LinuxDerivationBuilder : DerivationBuilder, DerivationBuilderParams
             };
         }
 
-        outputRewrites.clear();
         auto builtOutputs = nix::registerOutputs(
             store,
             localSettings,
             *this,
             addedPaths,
             scratchOutputs,
-            outputRewrites,
             buildUser.get(),
             tmpDir,
             [this](const std::string & p) { return store.toRealPath(p); });
@@ -426,8 +394,6 @@ struct LinuxDerivationBuilder : DerivationBuilder, DerivationBuilderParams
         return builtOutputs;
     }
 };
-
-const std::filesystem::path LinuxDerivationBuilder::homeDir = "/homeless-shelter";
 
 DerivationBuilderUnique makeLinuxDerivationBuilder(
     LocalStore & store, std::unique_ptr<DerivationBuilderCallbacks> miscMethods, DerivationBuilderParams params)
