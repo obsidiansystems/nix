7985b6d19f256c4c3f657aeb15b58d95f9a01760
Artemis Tosini <artemis.tosini@obsidian.systems>
2026-05-13 14:52:18 -0400

    nix store add: Allow scanning dependencies

--- src/libstore/restricted-store.cc
+++ src/libstore/restricted-store.cc
@@ -277,7 +277,7 @@ ref<const ValidPathInfo> RestrictedStore::addToStoreScanning(
 {
     auto path = next->addToStoreFromDump(
         dump, name, dumpMethod, hashMethod, hashAlgo, queryAllValidPaths(), RepairFlag::NoRepair, true);
-
+    goal.addDependency(path);
     return queryPathInfo(path);
 }
 
